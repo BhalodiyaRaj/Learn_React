3665210d3be7b14134f37a358f76c9cdf4f1e0e6
Raj <rajbhalodiya212@gmail.com>
2024-08-18 12:42:33 +0530

    debug and improve

--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -4,11 +4,14 @@ import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
-import SignUp from "./SignUp";
+import Signup from "../pages/Signup";
 import Login from "./Login";
 import PostForm from "./post-form/PostForm";
 import PostCard from "./PostCard";
 import AuthLayout from "./AuthLayout";
+import Button from "./Button";
+import Input from "./Input";
+import Select from "./Select";
 
 export { 
     Header, 
@@ -17,9 +20,12 @@ export {
     Logo,
     LogoutBtn,
     RTE,
-    SignUp,
+    Signup,
     Login,
     PostForm,
     PostCard,
-    AuthLayout
+    AuthLayout,
+    Button,
+    Input,
+    Select
 };
--- MegaBlog/src/main.jsx
+++ MegaBlog/src/main.jsx
@@ -5,14 +5,14 @@ import "./index.css";
 import { Provider } from "react-redux";
 import store from "./store/store.js";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { AuthLayout } from "./components/index.js";
+import { AuthLayout, Login } from "./components/index.js";
 import Home from "./pages/Home.jsx";
 import Signup from "./pages/Signup.jsx";
 import AllPosts from "./pages/AllPosts.jsx";
 import AddPost from "./pages/AddPost.jsx";
 import EditPost from "./pages/EditPost.jsx";
 import Post from "./pages/Post.jsx";
-import Login from "./pages/Login.jsx";
+// import Login from "./pages/Login.jsx";
 
 const router = createBrowserRouter([
   {
